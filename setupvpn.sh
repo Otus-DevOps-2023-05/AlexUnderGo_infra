c045beb4cfd57ad2bf4f08e04e48df37622a3ae7
AlexUndrGo <alex.undr.b@gmail.com>
2023-06-25 00:18:10 +0300

    cloud bastion fix setupvpn.sh

--- setupvpn.sh
+++ setupvpn.sh
@@ -1,3 +1,14 @@
+cat <<EOF> setupvpn.sh
 #!/bin/bash
+sudo apt update && sudo apt upgrade
+sudo apt install wget vim curl gnupg2 software-properties-common apt-transport-https ca-certificates lsb-release
 sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
 deb https://repo.pritunl.com/stable/apt jammy main
+EOF
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+echo "deb http://security.ubuntu.com/ubuntu focal-security main" | sudo tee /etc/apt/sources.list.d/focal-security.list
+sudo apt update
+sudo apt install libssl1.1
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
+sudo apt update && sudo apt install pritunl mongodb-org
