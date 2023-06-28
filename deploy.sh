489a54a12c12dcb3137606e7cc20ecb2a5bb950e
AlexUndrGo <alex.undr.b@gmail.com>
2023-06-29 02:04:32 +0300

    YC & cloud-testapp

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+cat <<EOF> deploy.sh
+#!/bin/bash
+sudo apt update && sudo apt upgrade
+sudo apt install -y git
+git clone -b monolith https://github.com/express42/reddit.git
+cd reddit && bundle install
+sudo gem install -y bundler
+puma -d
+#ps aux | grep puma
+#Check wrok external_ip:port
+EOF
