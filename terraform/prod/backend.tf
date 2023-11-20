cd617429c61ce18fdc3780d71f8959ea75099d76
AlexUndrGo <alex.undr.b@gmail.com>
2023-11-20 22:57:17 +0300

    terraform-2

---
+++ terraform/prod/backend.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.35.0"
+    }
+  }
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "otus-bucket77"
+    region   = "ru-central1"
+    key      = "prod.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+provider "yandex" {
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+  service_account_key_file = var.service_account_key_file
+}
---
+++ terraform/stage/main.tf
@@ -0,0 +1,15 @@
+module "db" {
+  source          = "../modules/db"
+  public_key_path = var.public_key_path
+  db_disk_image   = var.db_disk_image
+  subnet_id       = var.subnet_id
+}
+
+module "app" {
+  source          = "../modules/app"
+  public_key_path = var.public_key_path
+  app_disk_image  = var.app_disk_image
+  subnet_id       = var.subnet_id
+  #Connect to database mongo
+  db_url = module.db.external_ip_address_db
+}
